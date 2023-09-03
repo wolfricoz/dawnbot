400f7618e0cfc1f2d4b552b604583e22e091bfd0
wolfricoz <ricoisdenaam@gmail.com>
2023-09-04 00:20:22 +0200

    Added dice and coin flip

---
+++ modules/tools.py
@@ -0,0 +1,47 @@
+import discord
+from discord import app_commands
+from discord.ext import commands
+import random
+
+
+class Tools(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @app_commands.command(name="dice", description="rolls a dice for you!")
+    async def dice(self, interaction: discord.Interaction, dicetype: int, amount: int = 1):
+        await interaction.response.defer(thinking=False, ephemeral=True)
+        if dicetype < 2:
+            interaction.followup.send("Please choose a dice with at least 2 sides!")
+            return
+        if dicetype > 1000:
+            interaction.followup.send("Please choose a dice with less than 1000 sides!")
+            return
+        if amount > 50:
+            interaction.followup.send("you can roll up to 50 dice!")
+            return
+        x = 0
+        results = []
+        while x < amount:
+            x += 1
+            results.append(random.randint(1, dicetype))
+        rm = map(str, results)
+        t = ", ".join(rm)
+        counted = sum(results)
+        await interaction.followup.send(f"The dice has been cast..")
+        await interaction.channel.send(f"Results for {interaction.user.mention} {amount}d{dicetype}: {t} (total: {counted})")
+
+
+    @app_commands.command(name="coinflip", description="flips a coin for you!")
+    async def coin(self, interaction: discord.Interaction):
+        await interaction.response.defer(thinking=False, ephemeral=True)
+        r = random.randint(1, 2)
+        await interaction.followup.send(f"The coin has been cast..")
+        if r == 1:
+            await interaction.channel.send(f"Heads!")
+        else:
+            await interaction.channel.send(f"Tails!")
+
+
+async def setup(bot):
+    await bot.add_cog(Tools(bot))
