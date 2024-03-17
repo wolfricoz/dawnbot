04bbb82aeed83925a5ef5a4e2d66dc7ee8856d8e
wolfricoz <ricoisdenaam@gmail.com>
2024-03-17 20:15:11 +0100

    Start of the dice system, added characters, weapons, armor, and an attack command

--- modules/Armor.py
+++ modules/Armor.py
@@ -27,6 +27,62 @@ class armor(commands.GroupCog):
         CombatSystem().create_armor(name, hp, ac, hitchance, modifier)
         await interaction.response.send_message(f"Armor set {name} created")
 
+    @app_commands.command(name="delete", description="deletes an armor set")
+    @app_commands.checks.has_permissions(manage_guild=True)
+    async def delete(self, interaction: discord.Interaction, name: str) -> None:
+        """
+        Delete an armor set
+        :param interaction:
+        :param name:
+        """
+        armors = CombatSystem().armor
+        if name.lower() not in armors.keys():
+            await interaction.response.send_message(f"Armor set {name} does not exist")
+            return
+        CombatSystem().remove_armor(name)
+        await interaction.response.send_message(f"Armor set {name} deleted")
+
+    @app_commands.command(name="list", description="lists all armor sets")
+    async def list(self, interaction: discord.Interaction) -> None:
+        """
+        List all armor sets
+        :param interaction:
+        """
+        armors = CombatSystem().armor
+        embed = discord.Embed(title="Armor sets")
+        for x in armors.keys():
+            embed.add_field(name=x, value=f"HP: {armors[x]['hp']}\n"
+                                          f"AC: {armors[x]['ac']}\n"
+                                          f"Hit chance: {armors[x]['hitchance']}\n"
+                                          f"Modifier: {armors[x]['modifier']}")
+        await interaction.response.send_message(embed=embed)
+
+    @app_commands.command(name="edit", description="edits an armor set")
+    @app_commands.checks.has_permissions(manage_guild=True)
+    async def edit(self, interaction: discord.Interaction, name: str, hp: int = None, ac: int = None, hitchance: int = None, modifier: str = None) -> None:
+        """
+        Edit an armor set
+        :param interaction:
+        :param name:
+        :param hp:
+        :param ac:
+        :param hitchance:
+        :param modifier:
+        """
+        armors = CombatSystem().armor
+        if name.lower() not in armors.keys():
+            await interaction.response.send_message(f"Armor set {name} does not exist")
+            return
+        if hp is not None:
+            armors[name]['hp'] = hp
+        if ac is not None:
+            armors[name]['ac'] = ac
+        if hitchance is not None:
+            armors[name]['hitchance'] = hitchance
+        if modifier is not None:
+            armors[name]['modifier'] = modifier
+        CombatSystem().update_armor(name, armors[name]['hp'], armors[name]['ac'], armors[name]['hitchance'], armors[name]['modifier'])
+        await interaction.response.send_message(f"Armor set {name} edited")
 
 async def setup(bot):
     await bot.add_cog(armor(bot))
